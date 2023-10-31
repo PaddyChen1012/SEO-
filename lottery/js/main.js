7f744a8ff91544d188c3e5f6349c95e0e14851c7
DESKTOP-K767DRA\Paddy <paddy_chen@atarroba.com>
2023-10-31 10:38:13 +0800

    抽獎卷出版

---
+++ lottery/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    $('.menu-btn').click(function() {
+        $(".menu").toggleClass("active");
+        $('.bi').toggleClass("bi-list");
+        $('.bi').toggleClass("bi-x-lg");
+    })
+
+});
